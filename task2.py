c8b281e79f07e72a01905963d5f7b7e60b146c08
holmsya <iholmsya@gmail.com>
2021-09-26 14:44:29 +0300

    Initial commit

---
+++ task2.py
@@ -0,0 +1,34 @@
+# Переменная - именнованная область памяти, 
+# к которой мы можем обратиться по имени и произвести действия с информацией, 
+# записанной в этой области памяти. 
+
+var = 1
+str = "hello, world"
+flag = True
+
+# num1 = 78
+# num2 = 90
+
+num1 = int(input("Введите первое число: "))
+num2 = int(input("Введите второе число: "))
+
+print(num1 ** num2)
+
+num3 = num1 + num2
+
+print(num3)
+
+num1 = num1 * num2
+
+print(num1)
+
+num1 = num1 + 1
+
+print(num1)
+
+print(num3 / num1)
+
+print(num3 % num1) # остаток от деления
+
+print(num3 // num1) # целая часть
+
